7167b81043801cae45ef1f34337b36707ab86380
kakia <kakiarus@gamil.com>
2023-10-24 21:49:09 +0200

    dash_app and static dataset updated

--- code/dashboard/dash_app.py
+++ code/dashboard/dash_app.py
@@ -3,81 +3,127 @@ import pandas as pd
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 import plotly.express as px
 import plotly.graph_objects as go
 import json
 import os
+import DB_loader
+from sqlalchemy import create_engine, text, inspect, Table
+import cbsodata
 
+# db = DB_loader()
 
 current_path = os.path.abspath(__file__)
 geo_path = os.path.join(os.path.dirname(current_path),"../data/geo.json")
-data_path = os.path.join(os.path.dirname(current_path),"../data/total_crimes_per_region&year.csv")
 geo = gpd.read_file(geo_path)
-data = pd.read_csv(data_path)
 
-merged_data = pd.merge(geo, data, left_on = "statcode", right_on = "region")
-merged_data.to_crs(epsg=4326,inplace=True)
-gdf = merged_data.copy()
-gdf['geoid'] = gdf.index.astype(str)
-gdf = gdf[['geoid', 'geometry', 'statnaam', 'crimes_count','year']]
+data_path = os.path.join(os.path.dirname(current_path),"../data/total_crimes_data.csv")
+crimes = pd.read_csv(data_path,usecols=[0,1,2,3,7])
+
+region_code = pd.read_csv(os.path.join(os.path.dirname(current_path),"../data/region_code.csv"))
+
+alldata = pd.merge(region_code, crimes, left_on = "Key", right_on = "region_code").drop(columns='Key')
+
+# Load the crime types dict for the interface
+with open(os.path.join(os.path.dirname(current_path),"../data/selected_crimes_codes.json"), 'r') as json_file:
+    crimes_codes = json.load(json_file)
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(className='row', children=[
     html.Div([
+        dcc.Dropdown(id="crime-type-selector", options=[
+        {'label': 'Theft', 'value': 'theft'},
+        {'label': 'Damage', 'value': 'damage'},
+        {'label': 'Total', 'value': 'total'}
+        ], value='total'),
         dcc.Graph(id="crime-heatmap"),
-        dcc.Slider(id="year-selector",min=2010,  max=2022, step=1,  marks={str(year): str(year) for year in range(2010, 2023)},  
+        dcc.Slider(id="year-selector",min=2010,  max=2022, step=1,  marks={str(year): str(year) for year in range(2012, 2023)},  
             value=2022 ),
     ], style={'display': 'inline-block', 'width': '59%','padding': '0 20'}),  
 
     html.Div([
-        dcc.Dropdown(id="crime-type-selector", options=[
-        {'label': 'Theft', 'value': 'theft'},
-        {'label': 'Property Destruction', 'value': 'property_destruction'},
-        {'label': 'Total', 'value': 'total'}
-    ], value='total'),
+        dcc.Dropdown(id="crime-subtype-selector", options=[], 
+                     searchable=True, multi=True),
         dcc.Graph(id="crime-linechart"),
     ], style={'display': 'inline-block', 'width': '37%'}), 
 ])
 
 @app.callback(
     Output("crime-heatmap", "figure"),
-    [Input("year-selector", "value")]
+    Output("crime-subtype-selector", "options"),
+    [Input("year-selector", "value"),
+     Input("crime-type-selector", "value")]
 )
 
 
-def update_crime_heatmap(selected_year=2022):
-    data = gdf[gdf["year"].str.contains(str(selected_year))]
-    fig = px.choropleth_mapbox(data, 
-                           geojson=data.__geo_interface__, 
-                           locations=data.geoid, 
-                           color="crimes_count", 
+def update_crime_heatmap(selected_year=2022, crime_type = "total"):
+    # Select year
+    data_year = alldata[alldata["period"].str.contains(str(selected_year))]
+    # Select crime types 
+    subtype_options = []
+    if crime_type == "total":
+        #subtype_options = [{'label': 'Total', 'value': 'total'}]
+        print(data_year.columns)
+    else:
+        subtype_codes = crimes_codes[crime_type]
+        data_year = data_year[data_year['crime_code'].isin(list(subtype_codes.keys()))]
+    # Construct the subtypes options of the selected crime type that will pass to the linechart
+        for code, label in subtype_codes.items():
+            option = {'label': label, 'value': code}
+            subtype_options.append(option)
+    # Process the data
+    data_year['period'] = pd.to_datetime(data_year['period'].str.replace('-','',regex=True),format='%Y%m').dt.to_period('M')
+    data_sum = data_year.groupby(['region_code', data_year['period'].dt.year])['crimes'].sum().reset_index()
+    data =  pd.merge(region_code, data_sum, left_on = "Key", right_on = "region_code").drop(columns='Key')
+    merged_data = pd.merge(geo, data, left_on = "statcode", right_on = "region_code")
+    merged_data.to_crs(epsg=4326,inplace=True)
+    gdf = merged_data.copy()
+    gdf['geoid'] = gdf.index.astype(str)
+    gdf = gdf[['geoid', 'geometry', 'region','period','crimes']]
+    # Visualization of the selected data
+    fig = px.choropleth_mapbox(gdf, 
+                           geojson=gdf.__geo_interface__, 
+                           locations=gdf.geoid, 
+                           color="crimes", 
                             mapbox_style="stamen-toner", 
-                            hover_name = "statnaam",
+                            hover_name = "region",
                             zoom=6,
                             featureidkey='properties.geoid',
                             center = {"lat":52.12,"lon":5.16},
                             color_continuous_scale = "sunset",
                             title=f"Crime Heatmap {selected_year}")
     fig.update_layout(margin={"r": 0, "t": 50, "l": 0, "b": 0}  )
-    #fig.update_layout(mapbox={"style": "stamen-toner"}, showlegend=False)
-    return fig
+    return fig, subtype_options
 
 @app.callback(
     Output("crime-linechart", "figure"),
     [Input("crime-heatmap", "clickData"),
-     Input("crime-type-selector", "value")]
+    Input('crime-subtype-selector','value'),
+    Input("year-selector", "value")]
 )
 
-def update_crime_linechart(clickData, crime_type='total'):
+def update_crime_linechart(clickData, crime_subtype, selected_year = 2022):
     #print(json.dumps(clickData, indent=2))
     if not clickData:
-        print("No selected data.")
+        print("No selected cities.")
         selected_city = ["Amsterdam"]
     else:
         selected_city = [point['hovertext'] for point in clickData['points']]
-    data = gdf[gdf["statnaam"].isin(selected_city)]  # & (gdf["type"] == crime_type)
-    fig = px.line(data, x="year", y="crimes_count", title=f"{crime_type.capitalize()} Crimes Count of {selected_city[0]} Over Years")
+    if not crime_subtype:
+        print("No selected subtypes.")
+        raise PreventUpdate
+    # Select city
+    data_city = alldata[alldata["region"].isin(selected_city)]
+    # Select subtypes
+    selected_types = crime_subtype 
+    data = data_city[data_city["crime_code"].isin(selected_types)]
+    selected_data = data[data["period"].str.contains(str(selected_year))]
+    data_sum = selected_data.groupby(['region_code','period'])['crimes'].sum().reset_index()
+    data =  pd.merge(region_code, data_sum, left_on = "Key", right_on = "region_code").drop(columns='Key')
+    fig = px.line(data, x="period", y="crimes", 
+                  title=f"Selected Crimes Count of {selected_city[0]} in Year {selected_year}")
     fig.update_layout(margin={"r": 0, "t": 50, "l": 10, "b": 0},
                       plot_bgcolor="white",
                       yaxis=dict(title=None) )
